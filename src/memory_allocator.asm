27a886486e7c0f14d0f77143f0da4095619b230b
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2024-03-07 01:41:11 +0530

    Update memory_allocator.asm

--- src/memory_allocator.asm
+++ src/memory_allocator.asm
@@ -4,8 +4,20 @@ section .data
     allocation_bitmap resb mem_pool_size / block_size
 
 section .text
+    global intialize_memory_allocator
     global allocate_memory_blocks
     global deallocate_memory_blocks
+
+
+intialize_memory_allocator:
+    mov ecx, mem_pool_size
+    mov edi, allocation_bitmap
+    xor eax, eax
+.loop:
+    mov[edi], al
+    add edi, block_size
+    loop .loop
+    ret
 
 allocate_memory_blocks:
     pushad
