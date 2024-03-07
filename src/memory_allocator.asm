89f6740c67400e88d8d0097be033a14fd3023834
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2024-03-07 14:50:23 +0530

    Update memory_allocator.asm

--- src/memory_allocator.asm
+++ src/memory_allocator.asm
@@ -26,6 +26,30 @@ all general-purpose registers
     mov esx, mem_pool_size/block_size
     xor ebx, ebx
 
+
+.find_block:
+    mov al, [edi]
+    cmp al, 0
+    jne .next_block
+    mov byte [edi], 1
+    add edi, block_size
+    dec edx
+    cmp edx, 0
+
+    je . allcation_complete
+    jmp .find_block
+
+
+.next_block
+    add edi, block_size
+    jmp .find_block
+
+
+.allcation_complete:
+    mov eax, edi
+    sub eax, mem_pool_size
+    ret
+
 calculate_index:
     cmp eax, esi
     je deallocate_block 
